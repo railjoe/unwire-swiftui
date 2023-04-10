68d1b43da6e7d6c47c3b8d4302e03672dd86bcd9
Jovan Stojanov <railjoe@gmail.com>
2023-04-10 13:30:52 +0200

    Initial project setup

---
+++ unwire-swiftui/unwire_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  unwire_swiftuiApp.swift
+//  unwire-swiftui
+//
+//  Created by Jovan Stojanov on 9.4.23..
+//
+
+import SwiftUI
+
+@main
+struct unwire_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
